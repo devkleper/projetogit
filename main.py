b3a81c018bc1172e34dc4e709577ef69f1c00b2e
devkleper <quimericohyper@gmail.com>
2026-04-09 01:36:49 -0300

    Ajustar versão do python

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 def process_message(message: str) -> None:
     print(f"message:{message}")
 
+
 def avaliar_mensagem_comando(mensagem: str):
     pass
 
-def teste():
-    pass
 
 def avaliar_mensagem_comando() -> None:
     pass
 
+
 def main():
+
+    comandos: list[str] = []
+
     project_name: str = "projetogit"
     print(f"Hello from {project_name}!")
 
-    texto_usuario = ' Ola mundo! '
+    texto_usuario = " Ola mundo! "
     avaliar_mensagem_comando(texto_usuario)
 
 
