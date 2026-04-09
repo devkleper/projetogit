69523819c47e893f6a0ef581dab69e80740793f5
deniseantoniocanto-cmd <deniseantoniocanto@gmail.com>
2026-04-09 01:28:44 +0100

    versão criada para verificar comandos

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+def avaliar_mensagem_comando(mensagem: str):
+
+    pass
+
+
 def teste():
     pass
 
 
 def main():
-
     project_name: str = "projetogit"
-
     print(f"Hello from {project_name}!")
+
+    texto_usuario = ' Ola mundo! '
+    avaliar_mensagem_comando(texto_usuario)
 
 
 if __name__ == "__main__":
